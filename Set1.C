fdeaef4b66f970976567d101490d827d2223d553
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-01-22 16:36:45 +0530

    Set1
    positive or negative

---
+++ Set1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the number is %d");
+scanf("%d",&a);
+if(a>0)
+printf("The number is positive");
+if(a<0)
+printf("The numbrer is negative");
+else
+printf("The number is zero");
+return 0;
+}
