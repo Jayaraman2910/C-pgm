5e3271e04d886624028f500d1045d4e4e9a4b912
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-01-29 08:58:46 +0000

    hello

---
+++ set1-7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i=1;
+int a;
+printf("how many times the word world print);
+scan("%d",&a);
+for(i=1,i<=a,i++)
+{
+printf("\nHELLO");
+}
+return 0;
+}
