07a0079985826675d392da92e99494eb955e2040
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-01-29 08:36:35 +0000

    largest number

--- set1-5.C
+++ set1-5.C
@@ -5,9 +5,9 @@ void main()
     int num2, num3, num4;
     clrscr();
     printf("Enter the values of num1, num2 and num3\n");
-    scanf("%d %d %d", &num1, &num2, &num3);
-    printf("num1 = %d\tnum2 = %d\tnum3 = %d\n", num1, num2, num3);
-    if (num1 > num2)
+    scanf("%d %d %d", &num2, &num3, &num4);
+    printf("num1 = %d\tnum2 = %d\tnum3 = %d\n", num2, num3, num4);
+    if (num2 > num3)
     {
         if (num2 > num4)
         {
