132adfe0c69a66e8f0ed7608f7fa60038aeaf311
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-01-31 14:43:46 +0530

    Create set1-9.c

---
+++ set1-9.c
@@ -0,0 +1,18 @@
+#include<stduio.h>
+#include<coniop.h>
+void main()
+{
+int i,j,n,k,sum=0;
+printf("\nter the integers values\n");
+scanf("%d,&d",&n&k);
+for(j=1,j<n,j++)
+{
+printf("\n",j);
+}
+for(i=1,i<k,i++)
+{
+sum=sum+i;
+}
+printf("\nThe sum of integers is%d",sum);
+getch();
+}
