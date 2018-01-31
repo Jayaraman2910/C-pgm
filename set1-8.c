ac6610a16919d368242166d39abc1366ff70eef8
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-01-31 14:12:58 +0530

    Update set1-8.c

--- set1-8.c
+++ set1-8.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-int i=1,num,sum=0;
-printf("enter the sum of natural numbers");
+int k=1,num,sum=0;
+printf("Enter the sum of natural numbers");
 scanf("%d",&num);
-while(i<=num)
+while(k<=num)
 {
-sum= sum+i;
-i++;
+sum= sum+k;
+k++;
 }
 printf("%d",sum);
 getch();
