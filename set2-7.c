5c952f1375d9ecde7957b8ddb8ab99a3f015b27d
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-07 14:11:08 +0530

    Create set2-7.c

---
+++ set2-7.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int e,f,g=0,i,d;
+scanf("%d",&a);
+d=e;
+for(i=1;a>10;i++)
+{
+f=e%10;
+e=e/10;
+g=g+pow(f,3);
+}
+c=c+pow(a,3);
+if(d==g)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+return 0;
+}
+
+}
