f9d19a9f993f709b0372dc0324253e8fe0058504
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-07 14:28:31 +0530

    Create set2-8.c

---
+++ set2-8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\nEnter the values of a and b");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("\n The addition is%d",c);
+getch();
+}
