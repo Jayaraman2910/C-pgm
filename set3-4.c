7d2a717fb44802f20addfb698d60e10ce10cebb0
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-09 16:17:25 +0530

    Create set3-4.c

---
+++ set3-4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a[25],i,j,n;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=0;i<=n-1;i++)
+{
+for(j=1;j<=n-i-1:j++)
+{
+temp=a[j];
+a[j]=a[j+1]
+a[j+1]=temp;
+ }
+  }
+  printf("The sorted array is...%d",a[i]);
+  getch();
+}
