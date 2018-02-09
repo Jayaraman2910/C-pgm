b16c9efc49d8e405a2aea01ea5ceb8bfa1c0e4f8
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-09 16:21:00 +0530

    Create set3-5.c

---
+++ set3-5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i,arr[n],sum=0,median;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    median=sum/n;
+    printf("%d",median);
+    return 0;
+    getch();
+}
