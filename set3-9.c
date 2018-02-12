28c9bd46058e816e14a2646a71188dfdad84dde0
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-12 13:56:04 +0530

    Create set3-9.c

---
+++ set3-9.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,hours;
+printf("\n Enter the hours");
+scanf("%d",&hours);
+c=hours;
+a=c/60;
+printf("\n The respected hours for given time is%d");
+b=a/60;
+printf("\n The respected minutes for the given time is%d");
+return 0;
+}
