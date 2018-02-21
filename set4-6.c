a20422aff8d7d7bfed435221f2c3832e95a5a2bc
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-21 14:35:58 +0530

    Create set4-6.c

---
+++ set4-6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+void main() 
+{
+char s[100];
+int c=0,i;
+printf("Enter the string:");
+scanf("%s",s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]>='0'&&s[i]<='9')
+c++;
+}
+printf("%d",c);
+getch();
+}
