406042c82bb7ad43258d74a2b8386aa82986afe9
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-21 14:49:33 +0530

    Rename set3-7.c to set4-7.c

---
+++ set4-7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\n Enter thr values of a b");
+scanf("%d%d",&a,&b);
+c=a;
+a=b;
+b=c;
+printf("\n The swaping number is%d %d",a,b);
+getch();
+}
