629f7e744610bab9cc6b8c489bc97b2d541b22ed
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-21 14:53:59 +0530

    Create set4-8.c

---
+++ set4-8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int d,e;
+  printf("Enter the two elements:");
+  scanf("%d\t%d",&a,&b);
+  d^=e;
+  b^=e;
+  d^=e;
+  printf("After swapping %d %d",d,e);
+  return 0;
+}
