812578ab3e03b2b85a5e5237b548e2d35b96d083
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-23 15:59:59 +0530

    Create set5-3.c

---
+++ set5-3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char i[432],j[567];
+printf("Enter the string");
+scanf("%s",i);
+printf("Enter the string");
+scanf("%s",j);
+printf("The string is....: %s%s",i,j);
+return 0;
+}
