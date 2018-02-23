9459561e68754aa645456f09a8ce1eb79bc6cab6
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-02-23 16:03:42 +0530

    Create set5-4.c

---
+++ set5-4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main() {
+ int a;
+ printf("enter the no.");
+ scanf("%d",&a);
+ if(a<10)
+ {
+ 	printf("yse with in the range %d",a);
+ }
+ else
+ {
+ 	printf("not with in the range");
+ }
+ getch();
+	return 0;
+}
