fa9693e6adea278a121069bddf95003e8a5d2b94
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-05 14:08:03 +0530

    Create set7-6.c

---
+++ set7-6.c
@@ -0,0 +1,17 @@
+##include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k;
+printf("entera number");
+scanf("%d",&n);
+if(123>6)
+{
+printf("prime no");
+}
+else
+{
+printf("not prime no"):
+}
+getch();
+}
