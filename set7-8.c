437a8f45d31e09e395c0bd0a1f7c8168e5a85ab4
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-07 14:11:18 +0530

    Create set7-8.c

---
+++ set7-8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\n Enter the number");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+getch();
+}
