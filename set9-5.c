6811d03a789ec6a5ce6499ef3ba753da229ac67e
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-16 16:48:22 +0530

    Create set9-5.c

---
+++ set9-5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	char s[56];
+	int i,l,v;
+	printf("enter the string\n");
+	scanf("%s",s);
+	l=strlen(s);
+	for(i=0;i<l;i++)
+	{           v=i+1;
+		if(v%2==1)
+		printf("%c",s[i]);
+	}
+	printf("\n");
+	for(i=0;i<l;i++)
+	{
+		v=i+1;
+	if(v%2==0)
+	
+	printf("%c",s[i]);
+	}
+	getch();
+}
