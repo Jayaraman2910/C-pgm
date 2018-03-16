ca9e62e5a517c22983fbabffc73c2f62f3ebd911
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-16 15:59:43 +0530

    Create set8-8.c

---
+++ set8-8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  printf("enter a number");
+  scanf("%d",&a);
+  if(a%13==0)
+  printf("yes");
+  else
+  printf("no");
+  getch();
+}
