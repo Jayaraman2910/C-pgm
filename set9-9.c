19d4ce216b889e419b732b602086a39d562e781d
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-19 16:00:37 +0530

    Create set9-9.c

---
+++ set9-9.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,j,temp,n;
+    char a[20];
+    printf("enter the number:");
+    scanf("%s",&a);
+    n=strlen(a);
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%c",a[i]);
+    }
+getch();
+}
