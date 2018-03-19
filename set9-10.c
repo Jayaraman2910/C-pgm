8ec6ba4f5a718722f02209d25d15dba518f6b320
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-19 16:05:28 +0530

    Create set9-10.c

---
+++ set9-10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+char a[50];
+int i,len;
+printf("Enter the Alpha numeric");
+scanf("%s",a);
+len=strlen(a);
+for(i=0;i<len;i++)
+{
+if(a[i]>='0'&&a[i]<='9')
+{
+printf("%c",a[i]);
+}
+}
+getch();
+}
