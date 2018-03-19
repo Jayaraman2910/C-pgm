cf25e99ac4288751027f939ec9d728c99e39bc8b
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-19 15:51:55 +0530

    Create set9-7.c

---
+++ set9-7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,rem,gcd;
+printf("entr two num\n");
+scanf("%d%d",&x,&y);
+do
+{
+rem=x%y;
+if(rem==0)
+break;
+x=y;
+y=rem;
+}
+while(rem!=0);
+gcd=y;
+printf("%d",gcd);
+getch();
+}
