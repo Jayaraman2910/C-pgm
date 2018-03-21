4deb7b76a8c8a065dd8aa83a0b31a70ae380a2b0
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-21 16:10:34 +0530

    Create set 10-3.c

---
+++ set 10-3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int t,a,b=273;
+printf("\n Enter the temp in celsius......");
+scanf("%d",&t);
+a=t+b;
+printf("\n Temp in kelvin......%d",a);
+getch();
+}
