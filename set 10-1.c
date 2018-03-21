688566c3ea3fc98324affc714ed4b0c10a1d69ae
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-21 15:53:42 +0530

    Create set 10-1.c

---
+++ set 10-1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int l,b,h,v;
+printf("\n Length....");
+scanf("%d",&l);
+printf("\n Breath....");
+scanf("%d",&b);
+printf("\n Height....");
+scanf("%d",&h);
+v=l*b*h;
+printf("\n volume is......%d",v);
+getch();
+}
