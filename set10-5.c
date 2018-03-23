7ca34e40e4d0325f11c7d8be6ae14801f60381f1
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-23 16:06:56 +0530

    Create set10-5.c

---
+++ set10-5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,i;
+printf("\n Enter the amount and time and intrest");
+scanf("%d%d%d",&a,&b,&c);
+i=(a*b*c)/100;
+printf("\n Intrest is.....%d",i);
+getch();
+}
