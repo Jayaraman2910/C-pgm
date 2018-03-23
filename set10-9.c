d82999057fcf7074d26cea4b95fea895594372ad
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-23 16:47:50 +0530

    Create set10-9.c

---
+++ set10-9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+  int a,b,c,d;
+  printf ("\n Enter A value");
+  scanf ("%d", &a);
+  printf ("\n Enter B value");
+  scanf ("%d", &b);
+  printf ("\n Enter C value");
+  scanf ("%d", &c);
+  d= (a*b)%c;
+  printf ("\n The result is..:%d", d);
+  getch();
+}
