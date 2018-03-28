56b7c2fd94246aa9179eafdcbc3372c131850b1b
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-28 16:07:13 +0530

    Create set12-2.c

---
+++ set12-2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#inculde<conio.h>
+void main()
+{
+int a,b,c[20],i;
+printf("\n Enter the number");
+scanf("%d",&a);
+printf("\n Search Element");
+scanf("%d",&b);
+printf("\nThe numbers are..:");
+for(i=0;i<a;i++)
+{
+scanf("%d",&c[i]);
+}
+for(i=0;i<a;i++)
+{
+if(c[i]==b)
+{
+printf("\n YES");
+}
+else
+{
+    printf("\n No");
+}
+}
+getch();
+}
