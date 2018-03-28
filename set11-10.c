a315c2e2cab3646f2ffc0dcf34b84108ac7d9c40
Jayaraman2910 <35687977+Jayaraman2910@users.noreply.github.com>
2018-03-28 15:56:51 +0530

    Create set11-10.c

---
+++ set11-10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,a=0;
+clrscr();
+printf("\n Enter the n value");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+a=a+i;
+}
+printf("\n addition is....%d",a);
+getch();
+}
